e0411b17ecad0f0cf9b91e4b179bd30d49a38d6d
shaswat-shukla-dev <shaswat.shukla.dev@gmail.com>
2026-03-03 19:03:01 +0530

    variable

---
+++ variable.py
@@ -0,0 +1,16 @@
+#strings
+first_name='shaswat'
+food='pizza'
+email='shaswat123@gmail.com'
+print(f'Hello {first_name}')
+print(f'You like {food}')
+print(f'My fake mail {email}')
+
+#Integers
+age=20
+quantity=3
+num_of_students=30
+print(f'you are {age} years old')
+print(f'You are buying {quantity} items')
+print(f'your class has {num_of_students} students')
+
