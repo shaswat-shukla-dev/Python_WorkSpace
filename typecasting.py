d76c010d02fddb172cca6d261f27bf8f4d140f3c
shaswat-shukla-dev <shaswat.shukla.dev@gmail.com>
2026-03-03 19:13:07 +0530

    typecasting

---
+++ typecasting.py
@@ -0,0 +1,17 @@
+name='Tom'
+name2=''
+age=21
+gpa=3.5
+is_student=True
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(is_student))
+gap=int(gpa)
+print(gpa)
+age=float(age)
+print(age)
+name=bool(name)
+print(name)
+name2=bool(name2)
+print(name2)
--- variable.py
+++ variable.py
@@ -14,3 +14,23 @@ print(f'you are {age} years old')
 print(f'You are buying {quantity} items')
 print(f'your class has {num_of_students} students')
 
+#Float
+price=10.99
+gpa=3.2
+distance=5.5
+print(f'The price is {price}')
+print(f'your gpa is {gpa}')
+print(f'You ran {distance}km')
+
+#Boolean
+is_student=False
+for_sale=True
+print(f'Are you a student ?:{is_student}')
+
+if for_sale:
+    print('That item is for sale')
+else:
+    print('That item is not available')
+    
+
+
